fec4b8940f671d52c60d920e2bb13f6726962ecb
Sazzadul Islam <psazzadul@gmail.com>
2026-09-25 22:44:38 +0600

    - Created the Customer, customerDetails Model

---
+++ Models/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CustomerManagementPractiseCS.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        public required string Gender { get; set; }
+
+        [StringLength(1000)]
+        public string? BioData { get; set; }
+
+        // This Indecates that the Customer Table has a One-To-Many Relaionship with CustomerDetail Table
+        public ICollection<CustomerDetail> Details { get; set; } = new List<CustomerDetail>();
+    }
+}
---
+++ Models/CustomerDetail.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CustomerManagementPractiseCS.Models
+{
+    public class CustomerDetail
+    {
+        public int Id {  get; set; }
+
+        // this is the foregn key that is connected to the Customer table
+        public int CustomerId {  get; set; }
+
+        // Thisd is a Logical Connectjion to Connect to the Customer this dosent create atable Field 
+        public Customer? Customer { get; set; }  
+
+        [Required]
+        [Phone]
+        public string? Phone { get; set; }
+
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        [StringLength(500)]
+        public string? ProfileImage { get; set; } // "?" allowes the content to be null
+
+        [DataType(DataType.Date)]
+        public DateTime? DateOfBirth { get; set; }
+
+        [StringLength(100)]
+        public string? City { get; set; }
+
+        [StringLength(100)]
+        public string? Country { get; set; }
+        
+
+        [Required]
+        [StringLength(250)]
+        public string Address { get; set; } = string.Empty; // "string.Empty" this allowes the Content to be empty 
+
+        public bool IsActive { get; set; }
+
+    }
+}
